c4017e75c46f4ee878405c463d98c966eb9c7ce9
pranay0124@msitprogram.net <pranay0124@msitprogram.net>
2018-09-25 20:19:15 +0530

    "check_style_score -> 24 of 24 passed. style score: 0/1 "

--- ADS 1 - Assignments/m2/CodeCamp_Percolation/Solution.java
+++ ADS 1 - Assignments/m2/CodeCamp_Percolation/Solution.java
@@ -1,25 +1,40 @@
 // public class Percolation {
-//    public Percolation(int n)                // create n-by-n grid, with all sites blocked
-//    public    void open(int row, int col)    // open site (row, col) if it is not open already
-//    public boolean isOpen(int row, int col)  // is site (row, col) open?
-//    public boolean isFull(int row, int col)  // is site (row, col) full?
-//    public     int numberOfOpenSites()       // number of open sites
-//    public boolean percolates()              // does the system percolate?
+//    public Percolation(int n)
+      /*create n-by-n grid, with all sites blocked*/
+//    public    void open(int row, int col)
+      /*open site (row, col) if it is not open already*/
+//    public boolean isOpen(int row, int col)
+      /*is site (row, col) open?*/
+//    public boolean isFull(int row, int col)
+      /*is site (row, col) full?*/
+//    public     int numberOfOpenSites()
+      /*number of open sites*/
+//    public boolean percolates()
+      /*does the system percolate?*/
 // }
-
-
 // You can implement the above API to solve the problem
-import java.util.Scanner;
-import java.util.Arrays;
 
+/**
+ * util package.
+ */
+import java.util.Scanner;
+/**
+ * Class for solution.
+ */
 public class Solution {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int size = Integer.parseInt(scan.nextLine());
-		Percolation p = new Percolation(size);
-		while (scan.hasNext()) {
-			p.open(scan.nextInt(), scan.nextInt());
-		}
-		System.out.println(p.percolates());
-	}
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //Zero Parameter Constructor.
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int size = Integer.parseInt(scan.nextLine());
+        Percolation p = new Percolation(size);
+        while (scan.hasNext()) {
+            p.open(scan.nextInt(), scan.nextInt());
+        }
+        System.out.println(p.percolates());
+    }
 }
