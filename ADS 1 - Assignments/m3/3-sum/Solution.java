3a8a6206ce8710108e649412bb3c79418c75a8ef
pranay0124@msitprogram.net <pranay0124@msitprogram.net>
2018-09-26 22:43:07 +0530

    "check_style_score -> 4 of 4 passed. style score: 1/1 "

--- ADS 1 - Assignments/m3/3-sum/Solution.java
+++ ADS 1 - Assignments/m3/3-sum/Solution.java
@@ -8,51 +8,51 @@ import java.util.Arrays;
  * Class for solution.
  */
 final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
-		//Zero Parameter Constructor.
-	}
-	/**
-	 * Main Function.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int size = scan.nextInt();
-		int[] array = new int[size];
-		for (int i = 0; i < size; i++) {
-			array[i] = scan.nextInt();
-		}
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //Zero Parameter Constructor.
+    }
+    /**
+     * Main Function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int size = scan.nextInt();
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = scan.nextInt();
+        }
 
-		int a = 0;
-		int b = 0;
-		int c = 0;
-		int head = 0;
-		int tail = 0;
-		int count = 0;
-		Arrays.sort(array);
-		for (int i = 0; i < size - 2; i++) {
-			a = array[i];
-			head = i + 1;
-			tail = size - 1;
-			while (head < tail) {
-				b = array[head];
-				c = array[tail];
-				if (a + b + c == 0) {
-					count++;
-					head += 1;
-					tail -= 1;
-				} else if (a + b + c < 0) {
-					head += 1;
-				} else {
-					tail -= 1;
-				}
-			}
-		}
-		System.out.println(count);
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        int head = 0;
+        int tail = 0;
+        int count = 0;
+        Arrays.sort(array);
+        for (int i = 0; i < size - 2; i++) {
+            a = array[i];
+            head = i + 1;
+            tail = size - 1;
+            while (head < tail) {
+                b = array[head];
+                c = array[tail];
+                if (a + b + c == 0) {
+                    count++;
+                    head += 1;
+                    tail -= 1;
+                } else if (a + b + c < 0) {
+                    head += 1;
+                } else {
+                    tail -= 1;
+                }
+            }
+        }
+        System.out.println(count);
 
-	}
+    }
 }
