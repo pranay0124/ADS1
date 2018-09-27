5ae45e62ffa20e830b3bea0ec060e103615caf9c
pranay0124@msitprogram.net <pranay0124@msitprogram.net>
2018-09-27 16:38:50 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- ADS 1 - Practice/m4/a6/4.2 Evaluate Postfix/Solution.java
+++ ADS 1 - Practice/m4/a6/4.2 Evaluate Postfix/Solution.java
@@ -1,43 +1,70 @@
 import java.util.Scanner;
-import java.util.LinkedList;
+// import java.util.LinkedList;
+class LinkedList {
+	class Node {
+		int data;
+		Node next;
+		Node(int data) {
+			this.data = data;
+		}
+	}
+
+	private Node head, tail;
+
+	void push(int n) {
+		Node newhead = head;
+		while (newhead != null && newhead.next != null) {
+			newhead = newhead.next;
+		}
+		newhead.next = new Node(n);
+	}
+
+	int pop() {
+		int data = tail.data;
+		Node newhead = head;
+		while (newhead.next != tail) {
+			newhead = newhead.next;
+		}
+		newhead.next = null;
+		return data;
+	}
+}
 class Solution {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		String[] tokens = scan.nextLine().split(" ");
-		LinkedList<Integer> list = new LinkedList<>();
+		LinkedList list = new LinkedList();
 		for (int i = 0; i < tokens.length; i++) {
 			if (!("+*-/").contains(tokens[i])) {
-				list.add(Integer.parseInt(tokens[i]));
+				list.push(Integer.parseInt(tokens[i]));
 			} else {
 				switch (tokens[i]) {
 				case "+" :
-					int a = list.removeLast();
-					int b = list.removeLast();
-					list.add(a + b);
+					int a = list.pop();
+					int b = list.pop();
+					list.push(a + b);
 					break;
 
 				case "-" :
-					a = list.removeLast();
-					b = list.removeLast();
-					list.add(a - b);
+					a = list.pop();
+					b = list.pop();
+					list.push(a - b);
 					break;
 
 				case "*" :
-					a = list.removeLast();
-					b = list.removeLast();
-					list.add(a * b);
+					a = list.pop();
+					b = list.pop();
+					list.push(a * b);
 					break;
 
 				case "/" :
-					a = list.removeLast();
-					b = list.removeLast();
-					list.add(a / b);
+					a = list.pop();
+					b = list.pop();
+					list.push(a / b);
 					break;
 				}
 			}
-
 		}
-		System.out.println(list.get(0));
-
+		System.out.println(list);
 	}
 }
