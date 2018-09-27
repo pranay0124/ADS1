3007d6799031bfe414bdcdd9e68312e6eb2eb880
pranay0124@msitprogram.net <pranay0124@msitprogram.net>
2018-09-27 15:08:30 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ ADS 1 - Assignments/m4/Balanced Paranthesis/Solution.java
@@ -0,0 +1,90 @@
+import java.util.Scanner;
+import java.util.Arrays;
+class Stack {
+	String[] stack;
+	int size;
+	Stack() {
+		stack = new String[50];
+		size = 0;
+	}
+
+	void push(String element) {
+		try {
+			stack[size] = element;
+			size++;
+		} catch (Exception e) {
+			resize();
+			push(element);
+		}
+	}
+
+	void resize() {
+		stack = Arrays.copyOf(stack, size *2);
+	}
+
+	boolean isEmpty() {
+		return size==0;
+	}
+
+	String pop() {
+		String item = stack[size-1];
+		stack[size-1] = null;
+		size--;
+		return item;
+	}
+}
+
+class Solution {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n = Integer.parseInt(scan.nextLine());
+		while(scan.hasNext()) {
+			String[] tokens = scan.nextLine().split("");
+			Stack stack = new Stack();
+			boolean flag = false;
+			for(int i=0;i<tokens.length;i++) {
+				if(tokens.equals("{") || tokens.equals("[") || tokens.equals("(")) {
+					stack.push(tokens[i]);
+				}
+
+				if(tokens[i].equals("}")) {
+					String str;
+					if(!stack.isEmpty()) {
+						str = stack.pop();
+						if(!str.equals("{")) {
+							flag = true;
+							break;
+						}
+					}
+				}
+
+				if(tokens[i].equals("]")) {
+					String str;
+					if(!stack.isEmpty()) {
+						str = stack.pop();
+						if(!str.equals("[")) {
+							flag = true;
+							break;
+						}
+					}
+				}
+
+				if(tokens[i].equals(")")) {
+					String str;
+					if(!stack.isEmpty()) {
+						str = stack.pop();
+						if(!str.equals("(")) {
+							flag = true;
+							break;
+						}
+					}
+				}
+			}
+			if(!flag && stack.isEmpty()) {
+				System.out.println("YES");
+			} else {
+				System.out.println("NO");
+			}
+		}
+	}
+}
