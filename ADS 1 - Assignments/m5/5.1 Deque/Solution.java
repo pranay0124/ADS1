759d2a43712bf8340ebb9463afca89763cca7cc5
pranay0124@msitprogram.net <pranay0124@msitprogram.net>
2018-09-30 22:33:53 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- ADS 1 - Assignments/m5/5.1 Deque/Solution.java
+++ ADS 1 - Assignments/m5/5.1 Deque/Solution.java
@@ -1,29 +1,46 @@
-import java.util.*;
+/**
+ * @author : Pranay Kumar Y.
+ * Date : 30th September,2018.
+ */
+import java.util.Scanner;
+/**
+ * Class for solution.
+ */
 public class Solution {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		Deque<Integer> queue = new Deque<>();
-		while (scan.hasNext()) {
-			String[] tokens = scan.nextLine().split(" ");
-			switch (tokens[0]) {
-			case "pushRight":
-				queue.pushRight(Integer.parseInt(tokens[1]));
-				break;
-			case "pushLeft":
-				queue.pushLeft(Integer.parseInt(tokens[1]));
-				break;
-			case "popRight":
-				queue.popRight();
-				break;
-			case "popLeft":
-				queue.popLeft();
-				break;
-			case "size":
-				System.out.println(queue.getSize());
-				break;
-			default :
-			}
-		}
-
-	}
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //Zero Parameter Constructor.
+    }
+    /**
+     * Main Function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Deque<Integer> queue = new Deque<>();
+        while (scan.hasNext()) {
+            String[] tokens = scan.nextLine().split(" ");
+            switch (tokens[0]) {
+            case "pushRight":
+                queue.pushRight(Integer.parseInt(tokens[1]));
+                break;
+            case "pushLeft":
+                queue.pushLeft(Integer.parseInt(tokens[1]));
+                break;
+            case "popRight":
+                queue.popRight();
+                break;
+            case "popLeft":
+                queue.popLeft();
+                break;
+            case "size":
+                System.out.println(queue.getSize());
+                break;
+            default :
+            }
+        }
+    }
 }
