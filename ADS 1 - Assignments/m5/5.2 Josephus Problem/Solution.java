0d646bb4bb1860cf7aad18bb54a373c81cea044d
pranay0124@msitprogram.net <pranay0124@msitprogram.net>
2018-09-30 22:57:00 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- ADS 1 - Assignments/m5/5.2 Josephus Problem/Solution.java
+++ ADS 1 - Assignments/m5/5.2 Josephus Problem/Solution.java
@@ -1,20 +1,44 @@
-import java.util.*;
-public class Solution {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		String testcases = scan.nextLine();
-		while (scan.hasNext()) {
-			String[] tokens = scan.nextLine().split(" ");
-			int person = Integer.parseInt(tokens[0]);
-			int gap = Integer.parseInt(tokens[1]);
-			Deque<Integer> queue = new Deque<>();
-			createCircle(queue, person);
-			queue.josephus(person, gap);
-		}
-	}
-	public static void createCircle(Deque<Integer> queue, int person) {
-		for (int i = 0; i < person; i++) {
-			queue.push(i);
-		}
-	}
+/**
+ * @author : Pranay Kumar Y.
+ * Date : 30th September, 2018.
+ */
+import java.util.Scanner;
+/**
+ * Class for solution.
+ */
+final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //Zero Parameter Constructor.
+    }
+    /**
+     * Main Function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String testcases = scan.nextLine();
+        while (scan.hasNext()) {
+            String[] tokens = scan.nextLine().split(" ");
+            int person = Integer.parseInt(tokens[0]);
+            int gap = Integer.parseInt(tokens[1]);
+            Deque<Integer> queue = new Deque<>();
+            createCircle(queue, person);
+            queue.josephus(person, gap);
+        }
+    }
+    /**
+     * Creates a circle.
+     *
+     * @param      queue   The queue
+     * @param      person  The person
+     */
+    public static void createCircle(final Deque<Integer> queue, final int person) {
+        for (int i = 0; i < person; i++) {
+            queue.push(i);
+        }
+    }
 }
