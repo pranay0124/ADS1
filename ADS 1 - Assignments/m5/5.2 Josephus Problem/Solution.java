5ee74b4abd9d387b730a19998e47eda43f6e04db
pranay0124@msitprogram.net <pranay0124@msitprogram.net>
2018-09-30 22:52:22 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

---
+++ ADS 1 - Assignments/m5/5.2 Josephus Problem/Solution.java
@@ -0,0 +1,112 @@
+import java.util.*;
+public class Solution {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String testcases = scan.nextLine();
+		while (scan.hasNext()) {
+			String[] tokens = scan.nextLine().split(" ");
+			int person = Integer.parseInt(tokens[0]);
+			int gap = Integer.parseInt(tokens[1]);
+			Deque<Integer> queue = new Deque<>();
+			createCircle(queue, person);
+			queue.josephus(person, gap);
+		}
+	}
+	public static void createCircle(Deque<Integer> queue, int person) {
+		for (int i = 0; i < person; i++) {
+			queue.push(i);
+		}
+	}
+}
+class Deque<Item> {
+	Node first = null;
+	Node last = null;
+	int size = 0;
+	String print = "";
+	class Node {
+		Item data;
+		Node next;
+		Node(Item data) {
+			this.data = data;
+		}
+	}
+	public boolean isEmpty() {
+		return size == 0;
+	}
+	public int getSize() {
+		return size;
+	}
+	public void push(Item element) {
+		Node oldleft = last;
+		last = new Node(element);
+		if (isEmpty()) {
+			first = last;
+			last.next = first;
+			size++;
+			return;
+		}
+		last.next = first;
+		oldleft.next = last;
+		size++;
+	}
+	public Item pop(Item person) {
+		Item d = first.data;
+		if (isEmpty()) {
+			return null;
+		}
+		int s = 0;
+		Node get = first;
+		Node prev = null;
+		while (s < size) {
+			if (get.data == person) {
+				if (first.data == person) {
+					first = first.next;
+					int g = 0;
+					Node l = first;
+					while (g < size - 2) {
+						l = l.next;
+						g++;
+					}
+					last = l;
+					last.next = first;
+				} else {
+					prev.next = get.next;
+				}
+
+				size--;
+				break;
+			}
+			prev = get;
+			get = get.next;
+			s++;
+		}
+		return d;
+	}
+	public void josephus(int person, int gap) {
+		Node head = first;
+		while (!isEmpty()) {
+			int c = 0;
+			while (c < gap - 1) {
+				head = head.next;
+				c++;
+			}
+			print += head.data+" ";
+			pop(head.data);
+			head = head.next;
+		}
+		print = print.trim();
+		System.out.println(print);
+	}
+	public void printList() {
+		Node n = first;
+		Node tnode = first.next;
+		String s = "";
+		s += n.data + " ";
+		while (tnode != first) {
+			s += tnode.data + " ";
+			tnode = tnode.next;
+		}
+		s = s.trim();
+		System.out.println(s);
+	}
+}
