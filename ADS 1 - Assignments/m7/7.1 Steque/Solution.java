c3bf7d4de1d9cd06ded1c12e2d1e62cda53b8144
pranay0124@msitprogram.net <pranay0124@msitprogram.net>
2018-10-01 11:07:46 +0530

    "check_style_score -> 0 of 2 passed. style score: 0/1 "

---
+++ ADS 1 - Assignments/m7/7.1 Steque/Solution.java
@@ -0,0 +1,103 @@
+import java.util.Scanner;
+class Steque {
+	Node head = null;
+	Node tail = null;
+	int size = 0;
+	class Node {
+		int data;
+		Node next;
+		Node() {}
+		Node(int data) {
+			this.data = data;
+		}
+	}
+	boolean isEmpty() {
+		return head == tail;
+	}
+	int getsize() {
+		return size;
+	}
+	/**/
+	void pushLeft(int element) {
+		Node oldright = head;
+		head = new Node(element);
+		if (size == 0) {
+			head = tail;
+			printList();
+			size++;
+			return;
+		}
+		head.next = oldright;
+		size++;
+		printList();
+	}
+	/**/
+	int popLeft() {
+		if (size == 0) {
+			System.out.println("Steque is empty.");
+			return -1;
+		}
+		int removedData = head.data;
+		head = head.next;
+		size--;
+		printList();
+		return removedData;
+	}
+	/**/
+	void pushRight(int element) {
+		Node oldtail = tail;
+		tail = new Node(element);
+		if (size == 0) {
+			head = tail;
+			tail.next = null;
+			size++;
+			return;
+		}
+		tail.next = null;
+		oldtail.next = tail;
+		size++;
+		printList();
+	}
+	/**/
+	void printList() {
+		Node tnode = head;
+		String str = "";
+		while (tnode != null) {
+			str += tnode.data + ", ";
+			tnode = tnode.next;
+		}
+		System.out.println(str);
+	}
+}
+
+class Solution {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int testcases = Integer.parseInt(scan.nextLine());
+		for (int i = 0; i < testcases; i++) {
+			Steque queue = new Steque();
+			while (scan.hasNext()) {
+				String line = scan.nextLine();
+				if (!line.isEmpty()) {
+					String[] tokens = line.split(" ");
+					switch (tokens[0]) {
+					case "push":
+						queue.pushLeft(Integer.parseInt(tokens[1]));
+						break;
+					case "pop":
+						queue.popLeft();
+						break;
+					case "enqueue":
+						queue.pushRight(Integer.parseInt(tokens[1]));
+						break;
+					default:
+					}
+				} else {
+					System.out.println();
+					break;
+				}
+
+			}
+		}
+	}
+}
