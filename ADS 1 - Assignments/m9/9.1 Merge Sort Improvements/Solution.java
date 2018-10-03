d2f59a22783992b29e4bc8314c4ddf5edfd1adc6
pranay0124@msitprogram.net <pranay0124@msitprogram.net>
2018-10-04 00:13:27 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- ADS 1 - Assignments/m9/9.1 Merge Sort Improvements/Solution.java
+++ ADS 1 - Assignments/m9/9.1 Merge Sort Improvements/Solution.java
@@ -4,146 +4,147 @@ import java.util.Arrays;
  * Class for insertion sort.
  */
 class InsertionSort {
-	
-	/**
-	 * { less function to check for minimum }.
-	 *
-	 * @param      inputone  The inputone
-	 * @param      inputtwo  The inputtwo
-	 *
-	 * @return     { boolean value }
-	 */
-	boolean less(final  String inputone, final String inputtwo) {
+    /**
+     * { less function to check for minimum }.
+     *
+     * @param      inputone  The inputone
+     * @param      inputtwo  The inputtwo
+     *
+     * @return     { boolean value }
+     */
+    boolean less(final  String inputone, final String inputtwo) {
 
-		//checks for the greatness using compareTo function
+        //checks for the greatness using compareTo function
 
-		return inputone.compareTo(inputtwo) < 0;
-	}
-	void exchange(final String[] inputarray,
-	              final int bigindex,
-	              final int smallindex) {
+        return inputone.compareTo(inputtwo) < 0;
+    }
+    void exchange(final String[] inputarray,
+                  final int bigindex,
+                  final int smallindex) {
 
-		//creates a temporary variable to store the bigindex value
+        //creates a temporary variable to store the bigindex value
 
-		String temp = inputarray[bigindex];
+        String temp = inputarray[bigindex];
 
-		//swap the indexe
+        //swap the indexe
 
-		inputarray[bigindex] = inputarray[smallindex];
+        inputarray[bigindex] = inputarray[smallindex];
 
-		inputarray[smallindex] = temp;
-	}
-	void sort(final String[] inputarray, final int start, final int end) {
-		for (int i = start; i <= end; i++) {
-			for (int j = i; j > start; j--) {
-				if (less(inputarray[j], inputarray[j - 1])) {
+        inputarray[smallindex] = temp;
+    }
+    void sort(final String[] inputarray, final int start, final int end) {
+        for (int i = start; i <= end; i++) {
+            for (int j = i; j > start; j--) {
+                if (less(inputarray[j], inputarray[j - 1])) {
 
-					//call the exchange function.
+                    //call the exchange function.
 
-					exchange(inputarray, j, j - 1);
-				} else {
-					break;
-				}
-			}
-		}
-	}
+                    exchange(inputarray, j, j - 1);
+                } else {
+                    break;
+                }
+            }
+        }
+    }
 }
 class Mergesort {
-	/**
-	 * { function for less }.
-	 *
-	 * @param      inputone  The inputone
-	 * @param      inputtwo  The inputtwo
-	 *
-	 * @return     { description_of_the_return_value }
-	 */
-	boolean less(final  String inputone, final String inputtwo) {
+    /**
+     * { function for less }.
+     *
+     * @param      inputone  The inputone
+     * @param      inputtwo  The inputtwo
+     *
+     * @return     { description_of_the_return_value }
+     */
+    boolean less(final  String inputone, final String inputtwo) {
 
-		//checks for the greatness using compareTo function
+        //checks for the greatness using compareTo function
 
-		return inputone.compareTo(inputtwo) < 0;
-	}
-	/**
-	 * { merge function }.
-	 *
-	 * @param      inputarray    The inputarray
-	 * @param      storingarray  The storingarray
-	 * @param      first         The first
-	 * @param      middle        The middle
-	 * @param      last          The last
-	 */
-	void merge(String[] inputarray, String[] storingarray, int first, int middle, int last) {
-		int subarr1 = first;
-		int subarr2 = middle + 1;
-		for (int k = first; k <= last; k++) {
-			if (subarr1 > middle) {
-				storingarray[k] = inputarray[subarr2++];
-			} else if (subarr2 > last) {
-				storingarray[k] = inputarray[subarr1++];
-			} else if (less(inputarray[subarr2], inputarray[subarr1])) {
-				storingarray[k] = inputarray[subarr2++];
-			} else {
-				storingarray[k] = inputarray[subarr1++];
-			}
-		}
-	}
-	/**
-	 * { sort function }.
-	 *
-	 * @param      inputarray    The inputarray
-	 * @param      storingarray  The storingarray
-	 * @param      first         The first
-	 * @param      last          The last
-	 */
-	void sort(String[] inputarray, String[] storingarray, int first, int last) {
-		// System.out.println(Arrays.toString(inputarray) + "called");
-		InsertionSort insertion = new InsertionSort();
-		if (last - first <= 7) {
-			insertion.sort(storingarray, first, last);
-			System.out.println("Insertion sort method invoked...");
-			return;
-		}
-		int middle = (first + last) / 2;
-		sort(storingarray, inputarray, first, middle);
-		sort(storingarray, inputarray, middle + 1, last);
-		if (less(inputarray[middle], inputarray[middle + 1])) {
-			for (int i = first; i <= last; i++) {
-               storingarray[i] = inputarray[i];
-			}
-			System.out.println("Array is already sorted. So, skipped the call to merge...");
-			return;
-		}
-		merge(inputarray, storingarray, first, middle, last);
-	}
+        return inputone.compareTo(inputtwo) < 0;
+    }
+    /**
+     * { merge function }.
+     *
+     * @param      inputarray    The inputarray
+     * @param      storingarray  The storingarray
+     * @param      first         The first
+     * @param      middle        The middle
+     * @param      last          The last
+     */
+    void merge(String[] inputarray, String[] storingarray, int first, int middle, int last) {
+        int subarr1 = first;
+        int subarr2 = middle + 1;
+        for (int k = first; k <= last; k++) {
+            if (subarr1 > middle) {
+                storingarray[k] = inputarray[subarr2++];
+            } else if (subarr2 > last) {
+                storingarray[k] = inputarray[subarr1++];
+            } else if (less(inputarray[subarr2], inputarray[subarr1])) {
+                storingarray[k] = inputarray[subarr2++];
+            } else {
+                storingarray[k] = inputarray[subarr1++];
+            }
+        }
+    }
+    /**
+     * { sort function }.
+     *
+     * @param      inputarray    The inputarray
+     * @param      storingarray  The storingarray
+     * @param      first         The first
+     * @param      last          The last
+     */
+    void sort(String[] inputarray, String[] storingarray, int first, int last) {
+        // System.out.println(Arrays.toString(inputarray) + "called");
+        InsertionSort insertion = new InsertionSort();
+        if (last - first <= 7) {
+            insertion.sort(storingarray, first, last);
+            System.out.println("Insertion sort method invoked...");
+            return;
+        }
+        int middle = (first + last) / 2;
+        sort(storingarray, inputarray, first, middle);
+        sort(storingarray, inputarray, middle + 1, last);
+        if (less(inputarray[middle], inputarray[middle + 1])) {
+            for (int i = first; i <= last; i++) {
+                storingarray[i] = inputarray[i];
+            }
+            System.out.println("Array is already sorted."
+                +" So, skipped the call to merge...");
+            return;
+        }
+        merge(inputarray, storingarray, first, middle, last);
+    }
 }
 
 /**
  * Class for solution.
  */
 final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	Solution() {
-		//unused
-	}
-	/**
-	 * { main function }.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
-		Mergesort sortobj = new Mergesort();
-		while (scan.hasNext()) {
-			String line = scan.nextLine();
-			String[] tokens = line.split(",");
-			int low = 0;
-			int high = tokens.length - 1;
-			String[] temparray =  Arrays.copyOf(tokens, tokens.length);
-			sortobj.sort(temparray, tokens, low, high);
-			System.out.println(Arrays.toString(tokens));
-			System.out.println();
-		}
-	}
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //unused
+    }
+    /**
+     * { main function }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Mergesort sortobj = new Mergesort();
+        while (scan.hasNext()) {
+            String line = scan.nextLine();
+            String[] tokens = line.split(",");
+            int low = 0;
+            int high = tokens.length - 1;
+            String[] temparray =  Arrays.copyOf(tokens,
+                                    tokens.length);
+            sortobj.sort(temparray, tokens, low, high);
+            System.out.println(Arrays.toString(tokens));
+            System.out.println();
+        }
+    }
 }
