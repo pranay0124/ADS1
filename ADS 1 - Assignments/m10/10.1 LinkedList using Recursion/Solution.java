5c37b38b58b8e346291b78c2cb708620daa16663
pranay0124@msitprogram.net <pranay0124@msitprogram.net>
2018-10-04 18:43:23 +0530

    "ads1_m10_a1: 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS 1 - Assignments/m10/10.1 LinkedList using Recursion/Solution.java
@@ -0,0 +1,98 @@
+import java.util.Scanner;
+class Linkedlist {
+	Node head = null;
+	Node test;
+	Node prev;
+	Node temp;
+	int size = 0;
+	class Node {
+		int nodedata;
+		Node next;
+		Node(int data) {
+			this.nodedata = data;
+		}
+	}
+	void insertAt(int index, int inputdata) throws Exception {
+		if (index > size || index < 0) {
+			throw new Exception("Can't insert at this position.");
+			// return;
+		}
+		if (index == 1) {
+			Node newnode = new Node(inputdata);
+			newnode.next = test.next;
+			test.next = newnode;
+			test = head;
+			size++;
+			return;
+		}
+		if (index == 0) {
+			Node newnode = new Node(inputdata);
+			newnode.next = test;
+			head = newnode;
+			test =  head;
+			size++;
+			return;
+		}
+		test = test.next;
+		index--;
+		insertAt(index, inputdata);
+	}
+	void reverse() throws Exception {
+		if (size == 0) {
+			throw new Exception("No elements to reverse.");
+		}
+		if (test.next == null) {
+			head = test;
+			head.next = prev;
+			test = head;
+			prev = null;
+			return;
+		}
+		temp = test;
+		test = test.next;
+		temp.next = prev;
+		prev = temp;
+		reverse();
+	}
+	void print() {
+		String str = "";
+		Node newtest = head;
+		while (newtest.next != null) {
+			str = str + newtest.nodedata + ", ";
+			newtest = newtest.next;
+		}
+		str = str + newtest.nodedata;
+		System.out.println(str);
+	}
+}
+class Solution {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		Linkedlist linked = new Linkedlist();
+		while (scan.hasNext()) {
+			String line = scan.nextLine();
+			String[] tokens = line.split(" ");
+			switch (tokens[0]) {
+			case "insertAt":
+				try {
+					linked.insertAt(Integer.parseInt(tokens[1]), Integer.parseInt(tokens[2]));
+					linked.print();
+				} catch (Exception e) {
+					System.out.println(e.getMessage());
+				}
+
+				break;
+			case "reverse":
+				try {
+					linked.reverse();
+					linked.print();
+				} catch (Exception e) {
+					System.out.println(e.getMessage());
+				}
+				break;
+			default:
+			    break;	
+			}
+		}
+	}
+}
