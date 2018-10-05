ffae5be3fdfb35737c683440f6f2db291f39c8f4
pranay0124@msitprogram.net <pranay0124@msitprogram.net>
2018-10-05 15:12:27 +0530

    "ads1_m11_a1: 0/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS 1 - Assignments/m11/11.1 Cutoff Quicksort to Insertion Sort/Solution.java
+++ ADS 1 - Assignments/m11/11.1 Cutoff Quicksort to Insertion Sort/Solution.java
@@ -1,28 +1,56 @@
 import java.util.*;
+class InsertionSort {
+	boolean less(Comparable u, Comparable v) {
+		return u.compareTo(v) < 0;
+	}
+	void exch(Comparable[] a, int i, int j) {
+		Comparable temp = a[j];
+		a[j] = a[i];
+		a[i] = temp;
+	}
+	void sort(Comparable[] array) {
+		System.out.println("insertionSort called");
+		int n = array.length;
+		for (int i = 0; i < n; i++) {
+			for (int j = i; j > 0; j--) {
+				if (less(array[j], array[j - 1])) {
+					exch(array, j, j - 1);
+				} else {
+					break;
+				}
+			}
+		}
+	}
+}
 class QuickSort {
 	void swap(Comparable[] array, int a , int b) {
 		Comparable temp = array[a];
 		array[a] = array[b];
 		array[b] = temp;
 	}
-	void sort(Comparable[] array, int start, int end) {
+	void sort(Comparable[] array, int start, int end, int cutoff) {
 		if (start < end) {
+			if (end - start + 1 <= cutoff) {
+				InsertionSort is = new InsertionSort();
+				is.sort(array);
+				return;
+			}
 			int pivot = partition(array, start, end);
-			sort(array, start, pivot - 1);
-			sort(array, pivot + 1, end);
+			sort(array, start, pivot - 1, cutoff);
+			sort(array, pivot + 1, end, cutoff);
 		}
 	}
 	int partition(Comparable[] array, int start, int end) {
-		Comparable pivot = array[end];
+		Comparable pivot = array[start];
 		int i = start - 1;
-		for (int j = start; j < end; j++) {
-			if (array[j].compareTo(pivot)<= 0) {
+		for (int j = start; j < end + 1; j++) {
+			if (array[j].compareTo(pivot) <= 0) {
 				i++;
 				swap(array, i, j);
 			}
 		}
-		swap(array, i + 1, end);
-		return i + 1;
+		swap(array, i , start);
+		return i;
 	}
 }
 class Solution {
@@ -33,7 +61,7 @@ class Solution {
 			QuickSort qs = new QuickSort();
 			int cutoff = Integer.parseInt(scan.nextLine());
 			Comparable[] inptarray = scan.nextLine().split(" ");
-			qs.sort(inptarray, 0, inptarray.length - 1);
+			qs.sort(inptarray, 0, inptarray.length - 1, cutoff);
 			System.out.println(Arrays.toString(inptarray));
 		}
 	}
