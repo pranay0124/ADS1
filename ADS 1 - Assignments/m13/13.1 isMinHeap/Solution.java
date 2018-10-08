41c8d1b96dc864e25976294702f750ea635d6dc5
pranay0124@msitprogram.net <pranay0124@msitprogram.net>
2018-10-08 19:49:23 +0530

    "ads1_m13_a1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS 1 - Assignments/m13/13.1 isMinHeap/Solution.java
+++ ADS 1 - Assignments/m13/13.1 isMinHeap/Solution.java
@@ -1,100 +1,157 @@
+/**
+ * @author : Pranay Kumar Y.
+ * Date : 8th October,2018.
+ */
 import java.util.Scanner;
+/**==========================================================================
+ * Class for minimum heap.
+ *
+ * @param      <E>   { parameter_description }
+ *==========================================================================*/
 class MinHeap<E extends Comparable<E>> {
-	E[] heaparray;
-	int count;
-	int flag;
-	MinHeap(final int size) {
-		heaparray = (E[])new Comparable[size];
-		count = 0;
-		flag = 0;
-	}
-	boolean less(E valueone, E valuetwo) {
-		return valueone.compareTo(valuetwo) < 0;
-	}
-	boolean swim(E[] heaparray, int nodeval) {
-		while (nodeval > 1) {
-			if (less(heaparray[nodeval], heaparray[(nodeval - 1) / 2])) {
-				return false;
-			} else {
-				nodeval = (nodeval - 1) / 2;
-			}
-		}
-		return true;
-	}
-	int insert(E item) {
-		heaparray[count++] = item;
-		if (!swim(heaparray, count - 1)) {
-			flag = 1;
-		}
-		return flag;
-	}
+    /**
+     * { Heap Array }.
+     */
+    private E[] heaparray;
+    /**
+     * { count variable }.
+     */
+    private int count;
+    /**
+     * { flag variable }.
+     */
+    private int flag;
+    /**
+     * Constructs the object.
+     *
+     * @param      size  The size
+     */
+    MinHeap(final int size) {
+        heaparray = (E[])new Comparable[size];
+        count = 0;
+        flag = 0;
+    }
+    /**
+     * { function that returns the less value }.
+     *
+     * @param      valueone  The valueone
+     * @param      valuetwo  The valuetwo
+     *
+     * @return     { description_of_the_return_value }
+     */
+    boolean less(final E valueone, final E valuetwo) {
+        return valueone.compareTo(valuetwo) < 0;
+    }
+    /**
+     * { Swim function }.
+     *
+     * @param      heaparray  The heaparray
+     * @param      nodeval    The nodeval
+     *
+     * @return     { description_of_the_return_value }
+     */
+    boolean swim(final E[] heaparray, int nodeval) {
+        while (nodeval > 1) {
+            if (less(heaparray[nodeval], heaparray[(nodeval - 1) / 2])) {
+                return false;
+            } else {
+                nodeval = (nodeval - 1) / 2;
+            }
+        }
+        return true;
+    }
+    /**
+     * { Inserts the element }.
+     *
+     * @param      item  The item
+     *
+     * @return     { description_of_the_return_value }
+     */
+    int insert(final E item) {
+        heaparray[count++] = item;
+        if (!swim(heaparray, count - 1)) {
+            flag = 1;
+        }
+        return flag;
+    }
 }
-class Solution {
-	private Solution() {
-		//unused
-	}
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
-		String line = scan.nextLine();
-		int testnum = Integer.parseInt(scan.nextLine());
-		for (int i = 0; i < testnum; i++) {
-			outerloop:
-			switch (line) {
-			case "String":
-				String[] tokens = scan.nextLine().split(",");
-				MinHeap<String> stringobj = new MinHeap<String>(tokens.length);
-				for (int j = 0; j < tokens.length; j++) {
-					int returnval = stringobj.insert(tokens[j]);
-					if (returnval == 1) {
-						System.out.println("false");
-						break outerloop;
-					}
-				}
-				System.out.println("true");
-				break;
-			case "Integer":
-				tokens = scan.nextLine().split(",");
-				MinHeap<Integer> integerobj = new MinHeap<Integer>(tokens.length);
-				for (int j = 0; j < tokens.length; j++) {
-					int returnval = integerobj.insert(Integer.parseInt(tokens[j]));
-					if (returnval == 1) {
-						System.out.println("false");
-						break outerloop;
-					}
-				}
-				System.out.println("true");
-				break;
-			case "Double":
-				tokens = scan.nextLine().split(",");
-				MinHeap<Double> doubleobj = new MinHeap<Double>(tokens.length);
-				for (int j = 0; j < tokens.length; j++) {
-					int returnval = doubleobj.insert(Double.parseDouble(tokens[j]));
-					if (returnval == 1) {
-						System.out.println("false");
-						break outerloop;
-					}
-				}
-				System.out.println("true");
-				break;
-			case "Float":
-				tokens = scan.nextLine().split(",");
-				if(tokens[0].equals("")) {
-					System.out.println("false");
-					break;
-				}
-				MinHeap<Float> floatobj = new MinHeap<Float>(tokens.length);
-				for (int j = 0; j < tokens.length; j++) {
-					int returnval = floatobj.insert(Float.parseFloat(tokens[j]));
-					if (returnval == 1) {
-						System.out.println("false");
-						break outerloop;
-					}
-				}
-				System.out.println("true");
-				break;		
-			default:
-				break;
-			}
-		}
-	}
+/**==========================================================================
+ *              Class for Solution class.
+ *==========================================================================*/
+final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //unused
+    }
+    /**
+     * { Main Function }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String line = scan.nextLine();
+        int testnum = Integer.parseInt(scan.nextLine());
+        for (int i = 0; i < testnum; i++) {
+            outerloop:
+            switch (line) {
+            case "String":
+                String[] tokens = scan.nextLine().split(",");
+                MinHeap<String> stringobj = new MinHeap<String>(tokens.length);
+                for (int j = 0; j < tokens.length; j++) {
+                    int returnval = stringobj.insert(tokens[j]);
+                    if (returnval == 1) {
+                        System.out.println("false");
+                        break outerloop;
+                    }
+                }
+                System.out.println("true");
+                break;
+            case "Integer":
+                tokens = scan.nextLine().split(",");
+                MinHeap<Integer> integerobj = new MinHeap<Integer>(tokens.length);
+                for (int j = 0; j < tokens.length; j++) {
+                    int returnval = integerobj.insert(Integer.parseInt(tokens[j]));
+                    if (returnval == 1) {
+                        System.out.println("false");
+                        break outerloop;
+                    }
+                }
+                System.out.println("true");
+                break;
+            case "Double":
+                tokens = scan.nextLine().split(",");
+                MinHeap<Double> doubleobj = new MinHeap<Double>(tokens.length);
+                for (int j = 0; j < tokens.length; j++) {
+                    int returnval = doubleobj.insert(Double.parseDouble(tokens[j]));
+                    if (returnval == 1) {
+                        System.out.println("false");
+                        break outerloop;
+                    }
+                }
+                System.out.println("true");
+                break;
+            case "Float":
+                tokens = scan.nextLine().split(",");
+                if(tokens[0].equals("")) {
+                    System.out.println("false");
+                    break;
+                }
+                MinHeap<Float> floatobj = new MinHeap<Float>(tokens.length);
+                for (int j = 0; j < tokens.length; j++) {
+                    int returnval = floatobj.insert(Float.parseFloat(tokens[j]));
+                    if (returnval == 1) {
+                        System.out.println("false");
+                        break outerloop;
+                    }
+                }
+                System.out.println("true");
+                break;      
+            default:
+                break;
+            }
+        }
+    }
 }
