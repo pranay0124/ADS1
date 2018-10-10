4967395be0cfa1b2194cda04cc6a3aefea44e5b1
pranay0124@msitprogram.net <pranay0124@msitprogram.net>
2018-10-10 20:47:37 +0530

    "ads1_m15_a1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS 1 - Assignments/m15/15.1 BinarySearchSymbolTable/Solution.java
@@ -0,0 +1,186 @@
+import java.util.Scanner;
+class BinarySearchST<Key extends Comparable<Key>, Value> {
+    private Key[] keys;
+    private Value[]vals;
+    private int size = 0;
+
+    public BinarySearchST() {
+        keys = (Key[]) new Comparable[20];
+        vals = (Value[]) new Object[20];
+    }
+
+    public void put(final Key key, final Value value) {
+        if (key == null) {
+            System.out.println("key is null");
+        }
+        if (value == null) {
+            delete(key);
+            return;
+        }
+        int i = rank(key);
+        if (i < size && keys[i].compareTo(key) == 0) {
+            vals[i] = value;
+            return;
+        }
+        for (int j = size; j > i; j--) {
+            keys[j] = keys[j - 1];
+            vals[j] = vals[j - 1];
+        }
+        keys[i] = key;
+        vals[i] = value;
+        size++;
+    }
+
+    public int rank(final Key key) {
+        if (key == null) {
+            System.out.println("key is null");
+        }
+        int low = 0;
+        int high = size - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int compare = key.compareTo(keys[mid]);
+            if (compare < 0) {
+                high = mid - 1;
+            } else if (compare > 0) {
+                low = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return low;
+    }
+
+    public boolean contains(final Key key) {
+        if (key == null) {
+            System.out.println("key is null");
+        }
+        return get(key) != null;
+    }
+
+    public Value get(final Key key) {
+        if (key == null) {
+            System.out.println("key is null");
+        }
+        if (isEmpty()) {
+            return null;
+        }
+        int i = rank(key);
+        if (i < size && keys[i].compareTo(key) == 0) {
+            return vals[i];
+        }
+        return null;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public Key max() {
+        if (isEmpty()) {
+            System.out.println("No elements are available in the Symbol table");
+        }
+        return keys[size - 1];
+    }
+
+    public Key floor(final Key key) {
+        int i = rank(key);
+        if (i < size && key.compareTo(keys[i]) == 0) {
+            return keys[i];
+        }
+        if (i == 0) {
+            return null;
+        } else {
+            return keys[i - 1];
+        }
+    }
+
+    public void deleteMin() {
+        if (isEmpty()) {
+            System.out.println("No elements are available in the Symbol table to perform deletion");
+        }
+        delete(min());
+    }
+
+    public Key min() {
+        if (isEmpty()) {
+            System.out.println("empty symbol table");
+        }
+        return keys[0];
+    }
+
+    public void delete(final Key key) {
+        if (key == null) {
+            System.out.println("key is null");
+        }
+
+        int i = rank(key);
+        if (i == size || keys[i].compareTo(key) != 0) {
+            return;
+        }
+        for (int j = i; j < size - 1; j++)  {
+            keys[j] = keys[j + 1];
+            vals[j] = vals[j + 1];
+        }
+        size--;
+        keys[size] = null;
+        vals[size] = null;
+    }
+
+    public String keys() {
+        String str = "";
+        int i = 0;
+        for (i = 0; i < size - 1; i++) {
+            if (keys[i] != null) {
+                str += keys[i] + " " + vals[i] + "\n";
+            }
+        }
+        str += keys[size - 1] + " " + vals[size - 1];
+        return str;
+    }
+}
+
+final class Solution {
+    private Solution() {
+    }
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BinarySearchST bs = new BinarySearchST();
+        String line = sc.nextLine();
+        String[] tokens = line.split(" ");
+        for (int i = 0; i < tokens.length; i++) {
+            bs.put(tokens[i], i);
+        }
+        while (sc.hasNext()) {
+            String input = sc.nextLine();
+            String[] tokens1 = input.split(" ");
+            switch (tokens1[0]) {
+            case "get":
+                System.out.println(bs.get(tokens1[1]));
+                break;
+            case "max":
+                System.out.println(bs.max());
+                break;
+            case "floor":
+                System.out.println(bs.floor(tokens1[1]));
+                break;
+            case "rank":
+                System.out.println(bs.rank(tokens1[1]));
+                break;
+            case "contains":
+                System.out.println(bs.contains(tokens1[1]));
+                break;
+            case "deleteMin":
+                bs.deleteMin();
+                break;
+            case "keys":
+                System.out.println(bs.keys());
+                break;
+
+            default:
+                break;
+            }
+        }
+    }
+}
+
