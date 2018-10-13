f27876ebce42f37edf186fe81e8847f6b2c90d0f
pranay0124@msitprogram.net <pranay0124@msitprogram.net>
2018-10-13 13:31:27 +0530

    "ads1_week3_exam: 2/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS 1 - Assignments/m18/Week-3_Assignment-1/Solution.java
+++ ADS 1 - Assignments/m18/Week-3_Assignment-1/Solution.java
@@ -50,65 +50,65 @@ class Solution {
 
 			for (int i = 0; i < 5; i++) {
 				StockData mbest = maxpq.delMax();
-				// if (best.contains(mbest.getsname())) {
-				// 	best.put(mbest.getsname(), mbest.get(mbest.getsname()) + 1);
-				// } else {
-				// 	best.put(mbest.getsname(), 1);
-				// }
+				if (best.contains(mbest.getsname())) {
+					best.put(mbest.getsname(), best.get(mbest.getsname()) + 1);
+				} else {
+					best.put(mbest.getsname(), 1);
+				}
 				System.out.println(mbest.getsname() + " " + mbest.getschange());
 			}
 			System.out.println();
 			for (int j = 0; j < 5; j++) {
 				StockData mworst = minpq.delMin();
-				// if (worst.contains(mworst.getsname())) {
-				// 	worst.put(mworst.getsname(), mworst.get(mworst.getsname()) + 1);
-				// } else {
-				// 	worst.put(mworst.getsname(), 1);
-				// }
+				if (worst.contains(mworst.getsname())) {
+					worst.put(mworst.getsname(), worst.get(mworst.getsname()) + 1);
+				} else {
+					worst.put(mworst.getsname(), 1);
+				}
 				System.out.println(mworst.getsname() + " " + mworst.getschange());
 			}
 			System.out.println();
 			p++;
 		}
-		// int n1 = Integer.parseInt(scan.nextLine());
-		// while (n1 > 0) {
-		// 	String[] tokens = scan.nextLine().split(",");
-		// 	switch (tokens[0]) {
-		// 	case "get" :
-		// 		if (tokens[1].equals("maxST")) {
-		// 			if (maxpq.contains(tokens[2])) {
-		// 				System.out.println(maxpq.get(tokens[2]));
-		// 			} else {
-		// 				System.out.println("0");
-		// 			}
+		int n1 = Integer.parseInt(scan.nextLine());
+		while (n1 > 0) {
+			String[] tokens = scan.nextLine().split(",");
+			switch (tokens[0]) {
+			case "get" :
+				if (tokens[1].equals("maxST")) {
+					if (best.contains(tokens[2])) {
+						System.out.println(best.get(tokens[2]));
+					} else {
+						System.out.println("0");
+					}
 
-		// 		} else {
-		// 			if (minpq.contains(tokens[2])) {
-		// 				System.out.println(minpq.get(tokens[2]));
-		// 			} else {
-		// 				System.out.println("0");
-		// 			}
-		// 		}
+				} else {
+					if (worst.contains(tokens[2])) {
+						System.out.println(worst.get(tokens[2]));
+					} else {
+						System.out.println("0");
+					}
+				}
 
-		// 		break;
-		// 	case "intersection" :
-		// 		int maxpqSize = maxpq.size();
-		// 		int minpqSize = minpq.size();
-		// 		if (minpqSize > maxpqSize) {
-		// 			for (String each : minpq.keys()) {
-		// 				if (maxpq.contains(each)) {
-		// 					System.out.println(each);
-		// 				}
-		// 			}
-		// 		} else {
-		// 			for (String each : maxpq.keys()) {
-		// 				if (minpq.contains(each)) {
-		// 					System.out.println(each);
-		// 				}
-		// 			}
-		// 		}
-		// 		break;
-		// 	}
-		// }
+				break;
+			case "intersection" :
+				int bestSize = best.size();
+				int worstSize = worst.size();
+				if (worstSize > bestSize) {
+					for (String each : worst.keys()) {
+						if (best.contains(each)) {
+							System.out.println(each);
+						}
+					}
+				} else {
+					for (String each : best.keys()) {
+						if (worst.contains(each)) {
+							System.out.println(each);
+						}
+					}
+				}
+				break;
+			}
+		}
 	}
 }
