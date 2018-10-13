493b4551aa77295052fb01fee4c3c05a514a9fdd
pranay0124@msitprogram.net <pranay0124@msitprogram.net>
2018-10-13 11:29:15 +0530

    "ads1_week3_exam: 0/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS 1 - Assignments/m18/Week-3_Assignment-1/Solution.java
@@ -0,0 +1,57 @@
+import java.util.*;
+class StockData implements Comparable<StockData> {
+	private String stockname;
+	private double stockchange;
+	StockData(String name, double change) {
+		this.stockname = name;
+		this.stockchange = change;
+	}
+	public String getstockname() {
+		return this.stockname;
+	}
+	public double getstockchange() {
+		return this.stockchange;
+	}
+	public int compareTo(StockData other) {
+		if (this.stockchange > other.stockchange) {
+			return 1;
+		}
+		if (this.stockchange < other.stockchange) {
+			return -1;
+		}
+		return 0;
+	}
+	public String toString() {
+		String str = "";
+		str = str + this.stockname + this.stockchange;
+		return str;
+	}
+}
+class Solution {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		int p = 0;
+		while (p > 6) {
+			int count = 0;
+			MinPQ<StockData> minpq = new MinPQ<>();
+			MaxPQ<StockData> maxpq = new MaxPQ<>();
+			while (count < n) {
+				String[] tokens = scan.nextLine().split(",");
+				StockData stockobj = new StockData(tokens[0], Double.parseDouble(tokens[1]));
+				minpq.insert(stockobj);
+				maxpq.insert(stockobj);
+				count++;
+			}
+			for (StockData s1 : minpq) {
+				System.out.println(s1);
+			}
+			for (StockData s2 : maxpq) {
+				System.out.println(s2);
+			}
+
+			p++;
+		}
+
+	}
+}
