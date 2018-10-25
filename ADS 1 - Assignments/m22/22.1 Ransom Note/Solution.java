fcda3741276edb40c05a2ff04834f128c8e3b866
pranay0124@msitprogram.net <pranay0124@msitprogram.net>
2018-10-25 22:50:11 +0530

    "ads1_m22_a1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS 1 - Assignments/m22/22.1 Ransom Note/Solution.java
+++ ADS 1 - Assignments/m22/22.1 Ransom Note/Solution.java
@@ -154,7 +154,7 @@ class Queue<Item> implements Iterable<Item> {
         /**
          * Constructs the object.
          *
-         * @param      first  The first
+         * @param      first1  The first
          */
         ListIterator(final Node<Item> first1) {
             current = first1;
