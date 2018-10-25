0caec397766d82b514b8a7a45b1b98dce52de7c7
pranay0124@msitprogram.net <pranay0124@msitprogram.net>
2018-10-25 18:38:34 +0530

    "ads1_m22_a1: 5/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS 1 - Assignments/m22/22.1 Ransom Note/Solution.java
+++ ADS 1 - Assignments/m22/22.1 Ransom Note/Solution.java
@@ -7,21 +7,21 @@ class Solution {
 		String[] tokens = scan.nextLine().split(" ");
 		for (int i = 0; i < tokens.length; i++) {
 			if (inputHash.contains(tokens[i])) {
-				inputHash.put(tokens[i], inputHash.get(tokens[i]));
+				inputHash.put(tokens[i], inputHash.get(tokens[i]) + 1);
 			} else {
 				inputHash.put(tokens[i], 1);
 			}
 		}
 		String[] tokens1 = scan.nextLine().split(" ");
 		int count = tokens1.length;
-		System.out.println(count + "before");
+		// System.out.println(count + "before");
 		for (int j = 0; j < tokens1.length; j++) {
 			if (inputHash.contains(tokens1[j])) {
 				inputHash.delete(tokens1[j]);
 				count--;
 			}
 		}
-		System.out.println(count + "after");
+		// System.out.println(count + "after");
 		if (count <= 0) {
 			System.out.println("Yes");
 		} else {
