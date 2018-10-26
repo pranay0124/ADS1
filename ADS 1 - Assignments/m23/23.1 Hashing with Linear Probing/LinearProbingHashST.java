138e297dda71d5cedc6cb52344164621284f0e91
pranay0124@msitprogram.net <pranay0124@msitprogram.net>
2018-10-26 17:05:36 +0530

    "ads1_m23_a1: 4/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS 1 - Assignments/m23/23.1 Hashing with Linear Probing/LinearProbingHashST.java
+++ ADS 1 - Assignments/m23/23.1 Hashing with Linear Probing/LinearProbingHashST.java
@@ -209,9 +209,14 @@ public class LinearProbingHashST<Key, Value> {
                 str += keys[i] + ":" + vals[i] + ", ";
             }
         }
-        str = str.substring(0,str.length()-2);
-        str = str + "}";
-        System.out.println(str);
+        if (str != "{") {
+            str = str.substring(0, str.length() - 2);
+            str = str + "}";
+            System.out.println(str);
+        } else {
+            System.out.println("{}");
+        }
+
     }
 
 
